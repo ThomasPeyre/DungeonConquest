4d1c8ee51bf6ac6fd80d9e235a9cd92a6d598bae
ThomasPeyre <thomas.peyre@etu.univ-amu.fr>
2016-11-26 02:33:12 +0100

    HelloWorld

---
+++ DungeonConquest/src/main/java/dungeonConquest/HelloWorld.java
@@ -0,0 +1,33 @@
+package dungeonConquest;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+ 
+public class HelloWorld {
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800,600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+         
+        // init OpenGL here
+         
+        while (!Display.isCloseRequested()) {
+             
+            // render OpenGL here
+             
+            Display.update();
+        }
+         
+        Display.destroy();
+    }
+     
+    public static void main(String[] argv) {
+    	HelloWorld displayExample = new HelloWorld();
+        displayExample.start();
+    }
+}
